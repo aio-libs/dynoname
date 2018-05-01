7a762393d72ec76ed2228c5ea04291d5a9ede48a
Paul Colomiets <paul@colomiets.name>
2018-05-01 13:23:35 +0300

    Convertion from stdlib address to Address

--- dynoname/address.py
+++ dynoname/address.py
@@ -1,9 +1,13 @@
 from ipaddress import IPv4Address, IPv6Address
-from typing import List, Union
+from typing import List, Union, NewType
+import socket
 
 import attr
 
 IpAddress = Union[IPv4Address, IPv6Address]
+
+LocalAddress = NewType('LocalAddress', str)
+SingleAddress = Union[SocketAddr, LocalAddress]
 
 
 @attr.s
@@ -13,9 +17,30 @@ class SocketAddr:
 
 
 class Address:
-    def __init__(self):
-        pass
+    __slots__ = ('_first_priority',)
 
-    def diff(self, other: Address) -> (List[SocketAddr], List[SocketAddr]):
+    def diff(self, other: Address) -> (List[SingleAddress], List[SingleAddress]):
+        #
         return (new, old)
 
+    @classmethod
+    def from_getaddrinfo(list_of_addresses) -> Address:
+        first_priority = []
+        for (family, type, proto, canonname, sockaddr) in list_of_addresses:
+            if family == socket.AF_INET:
+                addr, port = sockaddr
+                first_priority.append(SocketAddr(
+                    ip=IPv4Address(addr),
+                    port=port,
+                ))
+            elif family == socket.AF_INET6:
+                addr, port, * = sockaddr
+                first_priority.append(SocketAddr(
+                    ip=IPv6Address(addr),
+                    port=port,
+                ))
+            else:
+                raise TypeError("Invalid address family")
+        me = Address()
+        me._first_priority = lst
+        return me
