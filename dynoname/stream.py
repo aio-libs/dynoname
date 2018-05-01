d8ebdf53749dcad1ef4d8d04e672c74f6a2e72e3
Paul Colomiets <paul@colomiets.name>
2018-05-01 15:46:24 +0300

    Implement updates for stdlib stream

--- dynoname/stream.py
+++ dynoname/stream.py
@@ -9,4 +9,13 @@ Stream = AsyncIterator[Address]
 
 async def stdlib_resolve(name: str, port: int) -> Address:
     lst = await asyncio.get_event_loop().getaddrinfo(name, port)
-    return Address.from_getaddrinfo(lst)
+    addr =  Address.from_getaddrinfo(lst)
+    yield addr
+    while True:
+
+        await asyncio.sleep(10)
+
+        lst = await asyncio.get_event_loop().getaddrinfo(name, port)
+        new_addr = Address.from_getaddrinfo(lst)
+        if new_addr != addr:
+            yield Address.from_getaddrinfo(lst)
