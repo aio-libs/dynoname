93eae03bca89d999ee70626910da4e5275510b52
Paul Colomiets <paul@colomiets.name>
2018-05-01 14:37:27 +0300

    Simplest stream

--- dynoname/stream.py
+++ dynoname/stream.py
@@ -1,6 +1,12 @@
+import asyncio
+from typing import AsyncIterator
+import socket
 
-async def stream():
-    yield await dns.resolve()
-    while True:
-        sleep()
-        yield await dns.resolve()
+from .address import Address
+
+Stream = AsyncIterator[Address]
+
+
+async def stdlib_resolve(name: str, port: int) -> Address:
+    lst = await asyncio.get_event_loop().getaddrinfo(name, port)
+    return Address.from_getaddrinfo(lst)
